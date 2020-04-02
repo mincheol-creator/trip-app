c6a9cc4e238a40cfcfa6ef953a3b7a540ef874ea
kkoomin <kkooming2@gmail.com>
2020-04-02 17:11:25 +0900

    product card 레이아웃 구성

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const ProductCard = () => {
-  return <div className="product-card">상품 카드</div>;
+const ProductCard = ({ tourData }) => {
+  const handleClick = () => {
+    alert("이동해야됨 ㅠ");
+  };
+  // console.log(tourData);
+  return (
+    <div className="product-card" onClick={handleClick}>
+      <div className="product-card-top">
+        <div className="product-card-top__photo">
+          {/* <img src={tourData.photo} alt="Tour" /> */}
+        </div>
+        {/* <div className="product-card-heart"></div> */}
+      </div>
+      <div className="product-card-bottom">
+        <div className="product-card-bottom__title">{tourData.name}</div>
+        <div className="product-card-bottom__review">
+          <span className="stars">⭐️ 4.5</span>
+          <span className="reviews">(200개의 후기)</span>
+        </div>
+        <div className="product-card-bottom__price">
+          {tourData.adult_price}원 / 1인
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- src/pages/city.pages.jsx
+++ src/pages/city.pages.jsx
@@ -15,8 +15,32 @@ class CityPage extends React.Component {
         country: "United Kingdom",
         desc:
           "There's no place like London! 🇬🇧 Fuga quasi enim molestiae ipsam et est ea praesentium. Ex eos corrupti dolores ut exercitationem quasi qui. Nisi quod eos facilis illum. Id quasi non quod et eius accusantium. Omnis mollitia nihil quia aut."
-      }
+      },
+      tourData: [
+        {
+          id: 3,
+          name: "런던 시내 워킹 투어",
+          description:
+            "단순한 흥미위주의 관광이 아닌, 역사적으로 좀 더 깊고 실질적으로 영국을 알아가는 현지/문화/체험투어",
+          start_date: "2020-02-07",
+          end_date: "2020-02-07",
+          available_start_date: "2020-01-01",
+          available_end_date: "2020-03-30",
+          adult_price: 35000,
+          youth_price: 30000,
+          location: "England SW1A 0AA London",
+          photo: "https://picsum.photos/200/300",
+          category: "tour",
+          city: "London",
+          country: "England",
+          createdAt: ""
+        }
+      ]
     };
+  }
+
+  componentDidMount() {
+    // city tour 랑 ticket 정보 불러오기
   }
 
   render() {
@@ -31,23 +55,23 @@ class CityPage extends React.Component {
             </div>
           </div>
         </section>
-        {/* <div className="city-desc">{desc}</div> */}
+        <div className="city-desc">{desc}</div>
         <section className="city-tour">
-          <div className="city-tour__title">가이드 투어</div>
+          <div className="city-tour__title">🚩{name_kor} 가이드 투어</div>
           <div className="city-tour__list">
+            <ProductCard tourData={this.state.tourData[0]} />
+            {/* <ProductCard />
             <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            <ProductCard /> */}
           </div>
         </section>
         <section className="city-ticket">
-          <div className="city-ticket__title">티켓</div>
+          <div className="city-ticket__title">🎫{name_kor} 티켓</div>
           <div className="city-ticket__list">
+            {/* <ProductCard />
             <ProductCard />
             <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            <ProductCard /> */}
           </div>
         </section>
       </div>
